c0583513e0c57efc5e9fdfcbf88f2106219f70d7
Anton Clayton <anton.clayton@gmail.com>
2025-03-29 00:25:16 -0700

    basic structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Grocery List Generator</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="bg-base-100 rounded-t-none p-2">
+                <li>
+                  <Link to="recipes">Recipes</Link>
+                </li>
+                <li>
+                  <Link to="/list">Grocery List</Link>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
