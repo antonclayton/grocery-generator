a9c41ddf9ce16e6763e433452830998a40b9535e
Anton Clayton <anton.clayton@gmail.com>
2025-03-29 22:17:55 -0700

    Add React Router Links to go from Home page to Recipes and GroceryList Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import GroceryList from "./pages/GroceryList";
+import Recipes from "./pages/Recipes";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/list" element={<GroceryList />} />
       </Routes>
     </div>
   );
