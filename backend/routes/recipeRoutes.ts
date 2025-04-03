fb8dca1261d00e04db2faf0de0e7bb9de85625ad
Anton Clayton <anton.clayton@gmail.com>
2025-04-02 20:12:32 -0700

    configure recipeRoutes to handle various controller requests

--- backend/routes/recipeRoutes.ts
+++ backend/routes/recipeRoutes.ts
@@ -5,6 +5,11 @@ import { requireAuthenticated } from "../middleware/authMiddleware";
 
 router.use(requireAuthenticated);
 
+// TODO pagination for this request
 router.get("/");
+router.get("/:id");
+router.post("/");
+router.delete("/:id");
+router.patch("/:id");
 
 export default router;
