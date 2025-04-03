5dce50b734b009d362077ca787703eff2b4138ed
Anton Clayton <anton.clayton@gmail.com>
2025-04-02 19:36:04 -0700

    add getAllIngredients functionality

--- backend/controllers/ingredientController.ts
+++ backend/controllers/ingredientController.ts
@@ -1,18 +1,30 @@
 import { NextFunction, Request, Response } from "express";
+import { IngredientModel } from "../models/ingredientModel";
+import { DatabaseError } from "../customErrors/DatabaseError";
 
-export const getAllIngredients = (
+export const getAllIngredients = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  try {
+    const ingredients = await IngredientModel.find(); // find all
+    res.status(200).json(ingredients);
+  } catch (error) {
+    console.log("Error getting ingredients: ", error);
+    next(new DatabaseError("Failed to fetch ingredients"));
+  }
+};
 
-export const createNewIngredient = (
+export const createNewIngredient = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const { name, category, userId } = req.body;
+};
 
-export const deleteIngredient = (
+export const deleteIngredient = async (
   req: Request,
   res: Response,
   next: NextFunction
