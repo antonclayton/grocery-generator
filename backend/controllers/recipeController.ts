f4d64f80f97e392f2f45376c301502008578260f
Anton Clayton <anton.clayton@gmail.com>
2025-04-02 20:55:24 -0700

    add routes and controller reqs for recipe categories

--- backend/controllers/recipeController.ts
+++ backend/controllers/recipeController.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
-import { RecipeModel } from "../models/recipeModel";
+import { RecipeCategoryModel, RecipeModel } from "../models/recipeModel";
 import { DatabaseError } from "../customErrors/DatabaseError";
 import { MongooseObjectIdError, NotFoundError } from "../customErrors";
 
@@ -10,9 +10,23 @@ export const getAllRecipes = async (
   res: Response,
   next: NextFunction
 ) => {
+  const page = parseInt(req.query.page as string) || 1; // default 1st page
+  const limit = parseInt(req.query.limit as string) || 10; // 10 recipe limit per page
   try {
-    const recipes = await RecipeModel.find().lean();
-    res.status(200).json(recipes);
+    const recipes = await RecipeModel.find()
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .lean();
+
+    // get total number of recipes
+    const totalRecipes = await RecipeModel.countDocuments();
+
+    res.status(200).json({
+      recipes,
+      totalRecipes,
+      totalPages: Math.ceil(totalRecipes / limit), // for use in the frontend
+      currentPage: page,
+    });
   } catch (error) {
     console.log("Error getting recipes: ", error);
     next(new DatabaseError("Failed to get all recipes"));
@@ -172,5 +186,80 @@ export const updateRecipe = async (
   } catch (error) {
     console.log("Error updating recipe");
     next(new DatabaseError("Failed to upadte recipe"));
+  }
+};
+
+// recipe categories
+export const getAllRecipeCategories = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const recipeCategories = await RecipeCategoryModel.find().lean();
+    res.status(200).json(recipeCategories);
+  } catch (error) {
+    console.log("Error getting recipe categories: ", error);
+    next(new DatabaseError("Failed to get all recipe categories"));
+  }
+};
+
+export const createNewRecipeCategory = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { name, userId } = req.body;
+
+  // make sure userId is valid
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    next(new MongooseObjectIdError("Recipe category's User ID is invalid"));
+    return;
+  }
+
+  try {
+    const newRecipe = new RecipeCategoryModel({
+      name,
+      userId,
+    });
+
+    const savedRecipe = await newRecipe.save();
+
+    res.status(200).json(savedRecipe);
+  } catch (error) {
+    console.log("Error creating recipe category: ", error);
+    next(new DatabaseError("Failed to create recipe category"));
+  }
+};
+
+export const deleteRecipeCategory = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    next(new MongooseObjectIdError("Invalid recipe category ID"));
+    return;
+  }
+
+  try {
+    // make sure recipe exists before deleting
+    const recipeCategory = await RecipeCategoryModel.findById(id);
+
+    if (!recipeCategory) {
+      next(new NotFoundError("Recipe category not found"));
+      return;
+    }
+
+    // find and delete
+    await RecipeCategoryModel.findByIdAndDelete(id);
+
+    // success
+    res.status(200).json({ message: "Recipe category deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting recipe category: ", error);
+    next(new DatabaseError("Failed to delete recipe category"));
   }
 };
--- backend/routes/recipeRoutes.ts
+++ backend/routes/recipeRoutes.ts
@@ -2,14 +2,29 @@ import express from "express";
 const router = express.Router();
 
 import { requireAuthenticated } from "../middleware/authMiddleware";
+import {
+  createNewRecipe,
+  createNewRecipeCategory,
+  deleteRecipe,
+  deleteRecipeCategory,
+  getAllRecipeCategories,
+  getAllRecipes,
+  getRecipeById,
+  updateRecipe,
+} from "../controllers/recipeController";
 
 router.use(requireAuthenticated);
 
-// TODO pagination for this request
-router.get("/");
-router.get("/:id");
-router.post("/");
-router.delete("/:id");
-router.patch("/:id");
+// recipe
+router.get("/", getAllRecipes); // TODO pagination for this request
+router.get("/:id", getRecipeById);
+router.post("/", createNewRecipe);
+router.delete("/:id", deleteRecipe);
+router.patch("/:id", updateRecipe);
+
+// recipe categories
+router.get("/categories", getAllRecipeCategories);
+router.post("/categories", createNewRecipeCategory);
+router.delete("/categories/:id", deleteRecipeCategory);
 
 export default router;
